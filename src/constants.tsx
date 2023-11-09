1516822e0e47f6e5d6d37b609bf2eb7fa5d44422
ramirezalberto <alramirez24@gmail.com>
2023-11-08 21:00:56 -0400

    uploading the website intro part

---
+++ src/constants.tsx
@@ -0,0 +1,3 @@
+export const NAME = "Alberto Ramirez Huaman"
+export const TITLE = "Finance Data & Analytics"
+export const DESCRIPTION = "Professional with Experience in Project Management, Finance Data Analyst and Software Programming. Check out my project and adaptable skills"
